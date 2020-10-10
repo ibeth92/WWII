2ebef494ed817cf064a46152e678c2ba24fc6da5
Ibet Hernandez <ibethernandez92@gmail.com>
2020-10-10 16:57:44 -0500

    updates

--- wwii_tables.sql
+++ wwii_tables.sql
@@ -1,4 +1,12 @@
--- Create some tables so that we can load information into them - this is the "Load" part of ETL
+CREATE TABLE aircraft_failure(
+	missiondate DATE,
+	latitude INT,
+	longitude INT,
+	weatherfails INT, 
+	mechanicalfails INT, 
+	miscfails INT
+	);
+    -- Create some tables so that we can load information into them - this is the "Load" part of ETL
 -- Create a table that creates an Event ID using dates given
 CREATE TABLE event_id(
 	id INT PRIMARY KEY,
@@ -16,26 +24,24 @@ FOREIGN KEY(event_id)
 	REFERENCES event_id(id)
 );
 
--- Create a table that gathers all useful weather data and organizes it by event id
-CREATE TABLE weather_data(
-	event_id INT,
-	Date DATE,
-	MAX INT, 
-	MIN INT,
-	MaxTemp INT, 
-	MinTemp INT,
-	Precip INT, 
-	WindGustSpd INT, 
-	Snowfall INT, 
-	PoorWeather INT, 
-PRIMARY KEY(Date),
-FOREIGN KEY(event_id)
-	REFERENCES event_id(id)
-);
-
 -- Create a table that organizes the weapons data which we will use to create a bomb id
 CREATE TABLE weapons_data(
 	bomb_id INT PRIMARY KEY,
 	WEAPON_NAME VARCHAR,
 	WEAPON_TYPE VARCHAR
+);
+-- Create a table that gathers all useful weather data and organizes it by event id
+CREATE TABLE weather_data(
+--  event_id INT,
+	date DATE,
+	maxtempc FLOAT(8), 
+	mintempc FLOAT(8),
+	maxtempf INT, 
+	mintempf INT,
+	precipitation VARCHAR, 
+	windgustspd FLOAT(8), 
+	snowfall FLOAT(8)
+--PRIMARY KEY(Date)
+--FOREIGN KEY(event_id)
+--	REFERENCES event_id(id)
 );
