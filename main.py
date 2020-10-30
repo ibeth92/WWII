ed646e3ebd0618b43eaa7edd93233489180f72b7
andrewzee23 <andrewazamora@gmail.com>
2020-10-30 13:42:24 -0500

    initializing main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+# This will be our Flask app
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/home/')
+def home():
+    return "Hello this is the main page <h1>Project 2</h1>"
+
+if __name__ == '__main__':
+    app.run()
