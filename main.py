be5807453c3359a19500007823083f9fba3f9e2e
andrewzee23 <andrewazamora@gmail.com>
2020-10-30 13:50:52 -0500

    added route (test)

--- main.py
+++ main.py
@@ -8,5 +8,9 @@ app = Flask(__name__)
 def home():
     return "Hello this is the main page <h1>Project 2</h1>"
 
+@app.route('/<name>/')
+def user(name):
+    return f'Hello {name}!'
+
 if __name__ == '__main__':
     app.run(debug = True)
