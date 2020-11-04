0614a730900d8b756823ecc050187fed06123f6a
andrewzee23 <andrewazamora@gmail.com>
2020-11-04 14:25:24 -0600

    creating database

--- database.py
+++ database.py
@@ -1,16 +1,4 @@
 import sqlite3
 from sqlite3 import Error
 
-def create_connection(db_file):
-    conn = None
-
-    try:
-        conn = sqlite3.connect(db_file)
-        print(sqlite3.version)
-    except Error as e:
-        print(e)
-    finally:
-        if conn:
-            conn.close()
-if __name__ == '__main__':
-    create_connection('wwii_db')
+conn = sqlite3.connect('wwii.db')
