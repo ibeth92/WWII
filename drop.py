d536def132836c8aa1bec1ce8b878f147eb5cc9e
andrewzee23 <andrewazamora@gmail.com>
2020-11-12 12:28:50 -0600

    updated wwii_data table

--- drop.py
+++ drop.py
@@ -8,15 +8,18 @@ conn = sqlite3.connect('wwii.db')
 #C reating a cursor object using the cursor() method
 cursor = conn.cursor()
 # Dropping weather table if already exists.
-cursor.execute("DROP TABLE IF EXISTS weapons_bombs;")
+cursor.execute("DROP TABLE IF EXISTS THOR_SCRAPE;")
 
-cursor.execute("DROP TABLE IF EXISTS weather;")
+cursor.execute("DROP TABLE IF EXISTS bombings;")
 # Creating table as per requirement
 # Create weather table
 
-cursor.execute("DROP TABLE IF EXISTS aircraft_failures;")
+cursor.execute("DROP TABLE IF EXISTS thor_failures;")
+cursor.execute("DROP TABLE IF EXISTS weather_data;")
+cursor.execute("DROP TABLE IF EXISTS weather_stations;")
+cursor.execute("DROP TABLE IF EXISTS wwii_data;")
 # Create aircraft failures table
-
+print('tables dropped')
 # Commit your changes in the database
 conn.commit()
 #Closing the connection
