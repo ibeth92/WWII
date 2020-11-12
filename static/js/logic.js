4f7c734828c491ec29bf77ff79b54215ca09301a
Vanessa Simpson <66039717+vanessasimpson@users.noreply.github.com>
2020-11-12 12:54:08 -0600

    logic update

--- static/js/logic.js
+++ static/js/logic.js
@@ -17,19 +17,49 @@ fetch(weapon_url)
             });
             console.log(mapevent);
 
-            function createMap(mapevent);
+            function createMap(mapevent) {
 
-            let myMap = L.map("map").setView([51.50, -0.09], 5);
+                let streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+                    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+                    tileSize: 512,
+                    maxZoom: 20,
+                    zoomOffset: -1,
+                    id: "mapbox/streets-v11",
+                    accessToken: API_KEY
+                });
 
-            let streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-                attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-                tileSize: 512,
-                maxZoom: 20,
-                zoomOffset: -1,
-                id: "mapbox/streets-v11",
-                accessToken: API_KEY
-            }).addTo(myMap);
+                let lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+                    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+                    maxZoom: 18,
+                    id: "light-v10",
+                    accessToken: API_KEY
+                });
 
+                let darkmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+                    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+                    maxZoom: 18,
+                    zoomOffset: -1,
+                    id: "dark-v10",
+                    accessToken: API_KEY
+                });
+
+                let baseMaps = {
+                    "Light Map": lightmap,
+                    "Street Map": streetmap,
+                    "Dark Map": darkmap
+                };
+
+                let bombMarkers = [];
+
+                function pointToLayer(mapevent) {
+                    let marker = L.Marker([mapLat, mapLon])
+                        .bindPopup("<h3>" + mapDate + <hr><h3>Location of Bomb Dropped: " + mapevent.LATITUDE + + mapevent.LONGITUDE"</h3>");
+                bombMarkers.push(marker);
+}
+
+                let myMap = L.map("map").setView([51.50, -0.09], 5,
+                    layers: [lightmap, bombMarkers]);
+}
 
             console.log(mapevent.DATE);
             console.log(mapevent.LATITUDE);
